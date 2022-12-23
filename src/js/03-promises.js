0e9879797cd0adcb072c729bf33fe21f48b1d869
MaksymTykhoniuk <mxtkhnk@gmail.com>
2022-12-23 20:40:26 +0200

    fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,19 +1,21 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
 
-form = document.querySelector('.form');
-delayInput = document.querySelector('input[name="delay"]');
-stepInput = document.querySelector('input[name="step"]');
-amountInput = document.querySelector('input[name="amount"]');
+const ref = {
+  form: document.querySelector('.form'),
+  delayInput: document.querySelector('input[name="delay"]'),
+  stepInput: document.querySelector('input[name="step"]'),
+  amountInput: document.querySelector('input[name="amount"]'),
+};
 
-form.addEventListener('submit', onsubmit);
+ref.form.addEventListener('submit', onsubmit);
 
 function onsubmit(evt) {
   evt.preventDefault();
 
-  const delay = +delayInput.value;
-  const step = +stepInput.value;
-  const amount = +amountInput.value;
+  const delay = +ref.delayInput.value;
+  const step = +ref.stepInput.value;
+  const amount = +ref.amountInput.value;
 
   for (let i = 1; i < amount + 1; i += 1) {
     const delayStep = +(delay + step * (i - 1));
